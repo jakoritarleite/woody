6e25fdf3d3faba4733824680234845ca18e234be
João Koritar <jakoritarleite@gmail.com>
2023-04-24 15:34:54 -0300

    Create macro for ecs Component

---
+++ crates/ecs/macros/src/lib.rs
@@ -0,0 +1,29 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn;
+use syn_path::path;
+
+#[proc_macro_derive(Component)]
+pub fn derive_component(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+
+    let struct_name = &ast.ident;
+
+    let path = path!(ecs);
+
+    TokenStream::from(quote! {
+        impl #path::component::Component for #struct_name {}
+
+        impl #path::component::Bundle for #struct_name {
+            fn components(
+                self,
+                storage: &mut #path::archetypes::ArchetypeStorage,
+                row_indexes: &mut impl FnMut(usize)
+            ) {
+                let row_index = storage.init_component(self);
+
+                row_indexes(row_index);
+            }
+        }
+    })
+}
